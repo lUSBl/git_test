eb16280d296a0794840885592a1a5e98cb2dc088
Kapelyuha Vladimir <perekkis@gmail.com>
2017-01-21 21:04:31 +0200

    add

---
+++ Job4j/chapter_001/task_1/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+/**
+* Class Calculate Task 1.
+* @author Vova Kapelyuha
+* @since  1.0
+*/
+public class Calculate{
+
+       /**
+	 * Prints 'Hello World!' to console.
+	 * @param args	String[]
+	 */
+    public static void main (String[] args){
+   	 System.out.println("Hello World");
+    }
+}
